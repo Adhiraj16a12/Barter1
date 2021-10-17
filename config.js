c58d91f3909a94b0b07053bc6b4ae94da62be2d4
Adhiraj16a12 <64204448+Adhiraj16a12@users.noreply.github.com>
2021-10-17 09:28:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0-q7MPnIyB5ldJpP7nPYb3VUZIumUYfg",
+  authDomain: "barter-67015.firebaseapp.com",
+  projectId: "barter-67015",
+  storageBucket: "barter-67015.appspot.com",
+  messagingSenderId: "488421564285",
+  appId: "1:488421564285:web:1a6b20c6bb8b393bd5b614",
+  measurementId: "G-WJVY13FWVM"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
